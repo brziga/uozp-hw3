6ea3bea1d5226998d3143c67ae27532fc49f60fc
giganizer <ziga.bradas@gmail.com>
2024-04-18 02:14:32 +0200

    TF-IDF for keywords of articles

--- hw3.py
+++ hw3.py
@@ -16,7 +16,7 @@ with open("rtvslo.yaml", "rt") as file:
 # TF-IDF
 
 # tf
-#   - raw count of a term in a document (kolikokrat se pojavi keyword pri nekem dokumenti - načeloma 1 tukaj)
+#   - raw count of a term in a document (kolikokrat se pojavi keyword pri nekem dokumentu - načeloma 1 tukaj)
 #   - denominator : koliko je vse skupaj kljucnih besed pri dokumentu
 #   - sepravi basically 1/(st klucnih besed)
 
@@ -32,4 +32,42 @@ with open("rtvslo.yaml", "rt") as file:
 #       - najprej notri tf
 #   - v locen pomozni dict se daje kljucne besede in steje, kolikokrat se pojavijo
 #   - se en obhod cez data, zdaj se v novi strukturi doda se idf in poracuna tf-idf
+
+dataTfidf = []
+keywordsIdf = {}
+numAllDocs = len(data)
+
+#cez vse clanke
+for i in range(len(data)):
+    vrstica = data[i]
+    dataTfidf[i] = {}
+    stKeywords = len(vrstica["gpt_keywords"])
+
+    #cez kljucne besede
+    for keyword in vrstica["gpt_keywords"]:
+
+        if keyword in keywordsIdf.keys():
+            keywordsIdf[keyword] += 1
+        else:
+            keywordsIdf[keyword] = 1
+
+        if keyword in dataTfidf[i].keys():
+            dataTfidf[i][keyword]["tf"] += 1
+        else:
+            dataTfidf[i][keyword] = {
+                "tf" : 1,
+                "idf" : None,
+                "tf-idf" : None
+            }
+    
+    for keyword in dataTfidf[i]:
+        dataTfidf[i][keyword]["tf"] /= stKeywords
+
+
+
+for i in range(len(data)):
+    vrstica = data[i]
+    for keyword in vrstica["gpt_keywords"]:
+        dataTfidf[i][keyword]["idf"] = keywordsIdf[keyword]
+        dataTfidf[i][keyword]["tf-idf"] = dataTfidf[i][keyword]["tf"] * dataTfidf[i][keyword]["idf"]
 
